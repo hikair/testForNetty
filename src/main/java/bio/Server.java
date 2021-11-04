61f2ad960ca1493878784271982e0a03052e315d
PengQian <351477856@qq.com>
2021-11-04 08:04:35 +0800

    bio

---
+++ src/main/java/bio/Server.java
@@ -0,0 +1,28 @@
+package bio;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        while(true) {
+            Socket client = serverSocket.accept(); // 阻塞
+            new Thread(() -> {
+                System.out.println(String.format("client connect success, port is %s", client.getPort()));
+                try {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    String msg;
+                    while ((msg = br.readLine()) != null) {
+                        System.out.println(msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).run();
+        }
+    }
+
+}
